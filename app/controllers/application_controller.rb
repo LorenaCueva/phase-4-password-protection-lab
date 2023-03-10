9e38070928a6c57398f20ef374988382ee0bcc92
Lorena Cueva <lorenacueva@gmail.com>
2023-03-10 14:08:24 -0800

    commit tree 148c733fc791e59ce819e64f2d2ba4627a91bc08

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+
+
+private
+
+  def render_record_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
 
 end
